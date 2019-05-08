79b5c48b077696061490b7c85caea923c9d7db02
arseo <ahrim2020@gmail.com>
2019-05-08 20:10:05 +0900

    1. 첫번째 페이지 그리드 만들기

--- app/src/main/java/com/nullja/nullja/PageOneFrag.java
+++ app/src/main/java/com/nullja/nullja/PageOneFrag.java
@@ -3,9 +3,15 @@ package com.nullja.nullja;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,21 +19,74 @@ import android.view.ViewGroup;
  */
 public class PageOneFrag extends Fragment {
 
+    private static final String TAG = "MainActivity";
+    private static final int NUM_COLUMNS = 2;
+
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<String> mNames = new ArrayList<>();
 
     public PageOneFrag() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page_one, container, false);
+
+        initImageBitmaps(view);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page_one, container, false);
+        return view;
     }
+
+
+    private void initImageBitmaps(View view){
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
+        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mNames.add("Havasu Falls");
+
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mNames.add("Trondheim");
+
+        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+        mNames.add("Portugal");
+
+        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
+        mNames.add("Rocky Mountain National Park");
+
+        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+        mNames.add("Mahahual");
+
+        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
+        mNames.add("Frozen Lake");
+
+        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
+        mNames.add("White Sands Desert");
+
+        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
+        mNames.add("Austrailia");
+
+        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
+        mNames.add("Washington");
+
+        initRecyclerView(view);
+    }
+
+    private void initRecyclerView(View view){
+        Log.d(TAG, "initRecyclerView: initializing staggered recyclerview.");
+
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
+        StaggeredRecyclerViewAdapter staggeredRecyclerViewAdapter =
+                new StaggeredRecyclerViewAdapter(getActivity(), mNames, mImageUrls);
+        // 정렬 방식 설정
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(NUM_COLUMNS, LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        recyclerView.setAdapter(staggeredRecyclerViewAdapter);
+    }
+
     public static PageOneFrag newInstance(){
         Bundle args = new Bundle();
-
         PageOneFrag fragment = new PageOneFrag();
         fragment.setArguments(args);
         return fragment;
