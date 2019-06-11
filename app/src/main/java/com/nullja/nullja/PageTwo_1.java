b9b1ee20c1962fbc72435d92ca9347a6718b7dd1
arseo <ahrim2020@gmail.com>
2019-06-11 16:08:51 +0900

    1. 2page form 만들어둠

--- app/src/main/java/com/nullja/nullja/PageTwo_1.java
+++ app/src/main/java/com/nullja/nullja/PageTwo_1.java
@@ -3,9 +3,14 @@ package com.nullja.nullja;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,15 +18,69 @@ import android.view.ViewGroup;
  */
 public class PageTwo_1 extends Fragment {
 
+    private static final String TAG = "MainActivity";
+
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.fragment_page_two_1);
+        Log.d(TAG, "onCreate: started.");
+
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page_two_1, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page_two_1, container, false);
+        initImageBitmaps(view);
+        return view;
     }
+
+    private void initImageBitmaps(View view){
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
+        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mNames.add("Havasu Falls");
+
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mNames.add("Trondheim");
+
+        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+        mNames.add("Portugal");
+
+        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
+        mNames.add("Rocky Mountain National Park aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+
+        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+        mNames.add("Mahahual");
+
+        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
+        mNames.add("Frozen Lake");
+
+
+        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
+        mNames.add("White Sands Desert");
+
+        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
+        mNames.add("Austrailia");
+
+        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
+        mNames.add("Washington");
+
+        initRecyclerView(view);
+    }
+
+    private void initRecyclerView(View view){
+        Log.d(TAG, "initRecyclerView: init recyclerview.");
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.recyclerv_view1);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getActivity(), mNames, mImageUrls);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
     public static PageTwo_1 newInstance(){
         Bundle args = new Bundle();
 
