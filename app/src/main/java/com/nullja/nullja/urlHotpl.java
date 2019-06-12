73be874c514457478500a8ccceb25c87a5875ad7
Nev3y3s <Nev3y3s@gmail.com>
2019-06-12 09:57:37 +0900

    insertData 추가

---
+++ app/src/main/java/com/nullja/nullja/urlHotpl.java
@@ -0,0 +1,23 @@
+package com.nullja.nullja;
+
+import android.graphics.Bitmap;
+
+import java.io.File;
+
+public class urlHotpl {
+    Integer hotplcat;
+    String hotplname;
+    String hotpladdr;
+    Double hotplat;
+    Double hotplon;
+    String hotplimage;
+}
+
+class imageHotpl {
+    Integer hotplcat;
+    String hotplname;
+    String hotpladdr;
+    Double hotplat;
+    Double hotplon;
+    Bitmap hotplimage;
+}
